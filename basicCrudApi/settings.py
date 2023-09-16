a35b592afdb3d43401ad16dea0a098926818a06c
Pundits-iTech <mohammedaalli088@gmail.com>
2023-09-16 10:05:55 +0000

    last commit

--- basicCrudApi/settings.py
+++ basicCrudApi/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+APPEND_SLASH=False
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
