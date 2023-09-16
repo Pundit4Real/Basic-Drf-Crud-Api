e2deaf059731bd01546e4c167674041928cae90f
Pundits-iTech <mohammedaalli088@gmail.com>
2023-09-16 08:42:17 +0000

    last commit

--- person/models.py
+++ person/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # persons class..
 class Person(models.Model):
     name = models.CharField(max_length=100, default='')
-    country = models.CharField(max_length=50,unique=True,default='')
+    country = models.CharField(max_length=50,default='')
     stack = models.CharField(max_length=30,default='')
     added = models.DateTimeField(auto_now_add=True)
 
